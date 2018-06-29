f8e2f5f232acef3afee398804a381afe25727d4a
lev <factor@mail.ru>
2018-06-29 13:14:42 +0700

    Последние изменения

---
+++ Bexs.dpr
@@ -0,0 +1,18 @@
+program Bexs;
+
+uses
+  OracleMonitor,
+  Forms,
+  ufMain in 'ufMain.pas' {FrmMain},
+  DataModule in 'DataModule.pas' {DataModule2: TDataModule},
+  ufEolink in 'ufEolink.pas' {FrmEolink};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TDataModule2, DataModule2);
+  DataModule2.OracleLogon1.Execute;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
